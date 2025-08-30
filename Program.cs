30e215684610869ef643f30bb6ecbf9b4317c012
Yogananda Rao Locherla <locherla@live.com>
2025-08-30 18:42:58 +1000

    Initial Commits to create Weather Alerts MCP Server

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http.Headers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(settings: null);
+
+builder.Services.AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+builder.Services.AddSingleton(static _ =>
+{
+    var client = new HttpClient() { BaseAddress = new Uri("https://api.weather.gov") };
+    client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("weather-tool", "1.0"));
+    return client;
+});
+
+var app = builder.Build();
+await app.RunAsync();
