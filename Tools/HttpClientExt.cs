24fc9c3f5d81878a1ea371ae8fe9db181c786fd0
Yogananda Rao Locherla <locherla@live.com>
2025-08-30 18:43:34 +1000

    Include HttpClientExtension file

---
+++ Tools/HttpClientExt.cs
@@ -0,0 +1,13 @@
+using System.Text.Json;
+
+namespace Weather.Tools;
+
+internal static class HttpClientExt
+{
+    public static async Task<JsonDocument> ReadJsonDocumentAsync(this HttpClient client, string requestUri)
+    {
+        using var response = await client.GetAsync(requestUri);
+        response.EnsureSuccessStatusCode();
+        return await JsonDocument.ParseAsync(await response.Content.ReadAsStreamAsync());
+    }
+}
